7e3be54b8e1887a40480de68a121c7ea32b6ee97
Alban Cavalli <alban.cavalli7@etu.univ-lorraine.fr>
2023-03-23 14:37:42 +0100

    img

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['www.google.com', 'images.google.com', 'thumblr.uniid.it', 'www.cdiscount.com', 'static.nike.com', 'www.picclickimg.com', 'i.ebayimg.com', 'www.tradeinn.com', 'images.footballfanatics.com', 'assets.adidas.com', 'images.puma.com', 'lh3.googleusercontent.com'],
+    domains: ['i.ibb.co', 'lh3.googleusercontent.com'],
   },
   env:{
     stripe_public_key: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY, 
--- pages/api/create-checkout-session.js
+++ pages/api/create-checkout-session.js
@@ -7,7 +7,7 @@ export default async (req, res) => {
     quantity: item.quantity,
     price_data: {
         currency: 'usd',
-        unit_amount: item.price * 100,
+        unit_amount: Math.round(item.price * 100),
         product_data: {
             name: item.title,
             description: item.description,
